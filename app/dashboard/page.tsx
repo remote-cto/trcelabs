fb069d61935459dabc8cb42c130ab675e54369d0
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2026-03-18 23:34:17 +0530

    FIX: Title issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Trcelabs | Advanced Cybersecurity Solutions",
+};
+
 const Page = () => {
   return (
     <>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import LoginForm from "../components/LoginForm";
 
-
+export const metadata = {
+  title: "Trcelabs | Advanced Cybersecurity Solutions",
+};
 export default function LoginPage() {
   return (
     <div>
