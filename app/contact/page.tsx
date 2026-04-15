351bb8092c4129093992a8c4de2e737e356addb3
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2026-04-16 00:40:15 +0530

    ADD: Contact Page

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: "Trcelabs | Advanced Cybersecurity Solutions",
+};
+
+const Page = () => {
+  return (
+    <>
+      <div style={{ width: "100%", height: "100vh" }}>
+        <iframe
+          src="/contact.html"
+          style={{ width: "100%", height: "100%", border: "none" }}
+          title="Trcelabs | Advanced Cybersecurity Solutions"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Page;
