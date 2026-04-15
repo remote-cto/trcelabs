335c3d6aa50f88b29093f5ba18a9d4a03ccc7fd4
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2026-04-15 22:17:04 +0530

    ADD: Products Page

---
+++ app/products/page.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: "Trcelabs | Advanced Cybersecurity Solutions",
+};
+
+const Page = () => {
+  return (
+    <>
+      <div style={{ width: "100%", height: "100vh" }}>
+        <iframe
+          src="/products.html"
+          style={{ width: "100%", height: "100%", border: "none" }}
+          title="DPDPA Readiness Assessment"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Page;
